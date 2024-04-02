80e7bfad57291e9d16a693380bfc2233a9c35852
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-02 20:21:32 -0300

    fix: questão 3

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -16,7 +16,7 @@ def multiplica_numeros():
         num1 = float(input("Digite o primeiro número: "))
         num2 = float(input("Digite o segundo número: "))
         produto = num1 * num2
-        print("A multiplicação entre números fornecidos é:", produto)
+        print("O produto dos números é:", produto)
     except:
         print("Entrada inválida")
 
