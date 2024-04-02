62375691db2eb19a0945019f0165827e05fe5810
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-02 20:09:20 -0300

    resolve questão 2

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -2,4 +2,14 @@ def saudar_usuario():
     nome = input("Qual é o seu nome? ")
     print("Olá, ", nome, "! Seja bem-vindo(a)!")
 
+def soma_numeros():
+    try:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        soma = num1 + num2
+        print("A soma dos números fornecidos é:", soma)
+    except:
+        print("Entrada inválida")
+
 saudar_usuario()
+soma_numeros()
